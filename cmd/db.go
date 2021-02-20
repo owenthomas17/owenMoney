d548949bf14f45e1396f6252e65e08348a05400c
owen t <2owen.thomas@gmail.com>
2021-02-20 13:16:03 +0000

    Move flag parsing implementation to its own package

--- cmd/db.go
+++ cmd/db.go
@@ -1,7 +1,9 @@
 package cmd
 
 import (
+	"flag"
 	"fmt"
+	"os"
 )
 
 func PrintHelp() {
@@ -11,4 +13,39 @@ db
 help
     `)
 
+}
+
+func DbFlagSet() *DbFlags {
+
+	dbFlags := &DbFlags{
+		fs: flag.NewFlagSet("db", flag.ExitOnError),
+	}
+
+	dbFlags.fs.StringVar(&dbFlags.dbName, "name", "default", "default DB filename")
+	dbFlags.fs.StringVar(&dbFlags.tblName, "table", "tbl_default", "default table name")
+
+	return dbFlags
+}
+
+type DbFlags struct {
+	fs      *flag.FlagSet
+	dbName  string
+	tblName string
+}
+
+func ProcessArgs(args []string) error {
+
+	switch args[0] {
+	case "db":
+		dbFlagSet := DbFlagSet()
+		if err := dbFlagSet.fs.Parse(args[1:]); err == nil {
+			fmt.Println(dbFlagSet.dbName)
+			fmt.Println(dbFlagSet.tblName)
+		}
+	case "help":
+		PrintHelp()
+		os.Exit(0)
+	}
+
+	return nil
 }
--- main.go
+++ main.go
@@ -1,46 +1,9 @@
 package main
 
 import (
-	"flag"
-	"fmt"
 	"os"
 	"owenMoney/cmd"
 )
-
-func DbFlagSet() *DbFlags {
-
-	dbFlags := &DbFlags{
-		fs: flag.NewFlagSet("db", flag.ExitOnError),
-	}
-
-	dbFlags.fs.StringVar(&dbFlags.dbName, "name", "default", "default DB filename")
-	dbFlags.fs.StringVar(&dbFlags.tblName, "table", "tbl_default", "default table name")
-
-	return dbFlags
-}
-
-type DbFlags struct {
-	fs      *flag.FlagSet
-	dbName  string
-	tblName string
-}
-
-func processArgs(args []string) error {
-
-	switch args[0] {
-	case "db":
-		dbFlagSet := DbFlagSet()
-		if err := dbFlagSet.fs.Parse(args[1:]); err == nil {
-			fmt.Println(dbFlagSet.dbName)
-			fmt.Println(dbFlagSet.tblName)
-		}
-	case "help":
-		cmd.PrintHelp()
-		os.Exit(0)
-	}
-
-	return nil
-}
 
 func main() {
 	args := os.Args
@@ -50,5 +13,5 @@ func main() {
 		os.Exit(1)
 	}
 
-	processArgs(args[1:])
+	cmd.ProcessArgs(args[1:])
 }
